f95d3a27caf30f88a679e3017514f80accc3f271
martinsillo <martinsillo.gamboa@gmail.com>
2015-09-09 14:22:48 -0500

    espacios en blanco

--- config.php
+++ config.php
@@ -1,22 +1,16 @@
 <?php
-
 //Parametros de Configuración
 $portal_activo = true;
 if(!$portal_activo){
   header('Location:under_const.php');
   exit;
 }
-
 if(!isset($index)){
-
 $host_bd = "localhost";
 $usuario_bd = "usuario";
 $clave_bd =  "clave";
 $bd = "padron_beneficiarios";
 $conexion=new mysqli($host_bd,$usuario_bd,$clave_bd,$bd);
 $conexion->query("SET NAMES utf8");
-
-
 }
-
 ?>
