5d83c251abdd22225a5bbede7b563ac50243f370
martinsillo <martinsillo.gamboa@gmail.com>
2015-09-10 16:22:08 -0500

    se agrega seleccion de grupo colectivo

--- modulos/ben_colectivo.php
+++ modulos/ben_colectivo.php
@@ -34,6 +34,26 @@ unset($_POST);
 <section class="content">
 <div class="box box-danger">
 <div class="panel-body">
+
+<form  method="post" id="rfc_form" name="rfc_form" role="form" action="?token=<?php print(md5(15)); ?>">
+<div class="form-group">
+                      <label>Agregar beneficairios a un grupo ya registrado:</label>
+                      <select class="form-control">
+                        <option>-seleccione-</option>
+                      <?php
+$consulta_colectivos = "select * from ben_colectivos_lista";
+$ex_col = $conexion->query($consulta_colectivos);
+while($r_col=$ex_col->fetch_assoc()){
+echo "<option value='".$r_col['c']."'>".$r_col['n']."-".$r_col['r']."</option>";
+}
+
+?>
+                      </select>
+                    </div>
+</form>
+
+
+
 Para poder registrar un nuevo grupo de beneficiarios colectivos primeramente ingrese el RFC del beneficario, esto para verificar que NO se encuentre registrado y continuar con el proceso de registro, en caso de estar registrado se mostrará la información del grupo para cotejamiento de la información.
 <br>
 <br>Ingrese el RFC:&nbsp;
